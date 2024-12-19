5761983686c8eeedf849ec5dc599d201d6891068
Zach Ross <zacharyross1234@gmail.com>
2024-12-19 15:53:09 -0500

    State nav & game cards

---
+++ SimpleGameTracker/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import GameCard from './components/GameCard.vue'
+
+import { ref } from 'vue'
+
+const state = ref('playing')
+
+</script>
+
+<template>
+  <header>
+    <nav>
+      <button id="wishlist-btn" :class="{ selected: state == 'wishlist' }" @click="state = 'wishlist'">Wishlist</button>
+      <button id="playing-btn" :class="{ selected: state == 'playing' }" @click="state = 'playing'">Playing</button>
+      <button id="completed-btn" :class="{ selected: state == 'completed' }" @click="state = 'completed'">Completed</button>
+    </nav>
+  </header>
+
+  <main>
+    <GameCard name="The Finals"/>
+    <GameCard name="Helldivers 2"/>
+    <GameCard name="Battlefield 2042"/>
+  </main>
+</template>
+
+<style scoped>
+
+  #wishlist-btn {
+    background-color: rgb(228, 71, 202);
+  }
+  #playing-btn {
+    background-color: rgb(79, 186, 248);
+  }
+  #completed-btn {
+    background-color: rgb(58, 223, 71);
+  }
+
+  main {
+    display: flex;
+    justify-content: center;
+  }
+
+</style>
---
+++ SimpleGameTracker/src/components/GameCard.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="game-card">
+    <div class="img"></div>
+    <h3>{{ name }}</h3>
+  </div>
+</template>
+
+<style scoped>
+.game-card {
+  flex-grow: 1 1 0;
+  width: 200px;
+  margin: 5px;
+  border: solid;
+  border-width: 2.5px;
+  border-radius: 10px;
+  text-align: center;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.game-card:hover {
+  scale: 0.99;
+}
+
+.game-card .img {
+  background-color: var(--vt-c-indigo);
+  width: 100%;
+  aspect-ratio: 1 / 1;
+}
+
+h3 {
+  text-wrap: nowrap;
+}
+</style>
