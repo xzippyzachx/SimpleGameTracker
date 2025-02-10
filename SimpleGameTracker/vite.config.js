ecef6095c1677ef5ce97be56287def6d4c665ff1
Zach Ross <zacharyross1234@gmail.com>
2025-02-10 16:36:30 -0500

    PWA support

--- SimpleGameTracker/vite.config.js
+++ SimpleGameTracker/vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -9,6 +10,44 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       vue(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['icons/favicon.ico', 'icons/favicon.svg', 'icons/apple-touch-icon.png'],
+        manifest: {
+          name: 'Simple Game Tracker',
+          short_name: 'SGT',
+          theme_color: '#000000',
+          background_color: "#181818",
+          icons: [
+            {
+              src: 'icons/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any'
+            },
+            {
+              src: 'icons/pwa-maskable-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'maskable'
+            },
+            {
+              src: 'icons/pwa-maskable-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable'
+            }
+          ]
+        },
+        devOptions: {
+          enabled: true
+        }
+      })
     ],
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
